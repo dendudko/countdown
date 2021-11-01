d01a90d3a7fbfd9d4aa06b27838a041e339d68b0
dendudko <dudko.do@students.dvfu.ru>
2021-11-01 20:34:06 +1000

    FFF

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 let date;
 let timeout;
-
+let dateToCompare = new Date(0, 0, 0, 0, 0, 0, 0);
 function out() {
     document.getElementById('timer').innerHTML = date.toLocaleTimeString();
 }
 async function timer(){
     let promise = new Promise((resolve) => {
         out();
-        if (date.getSeconds()!=0){
+        if (date > dateToCompare){
             date.setSeconds(date.getSeconds()-1);
             timeout = setTimeout(timer, 1000);
         }
         else{
-            resolve("Время вышло!");
+            resolve("Time is over... BOOM!!!");
         }
     })
-        .then(value => setTimeout(() => alert(value), 0));
+        .then(value => document.getElementById('timer').innerHTML = value);
 }
 
 function start(){
-    date = new Date(0, 0, 0, 0, 0, 10, 0);
+    let sec = document.getElementById("input").value;
+    date = new Date(0, 0, 0, 0, 0, sec, 0);
     clearTimeout(timeout);
     out();
     timer();
 }
+
+addEventListener('keydown', function(key) {
+    if (key.keyCode == 13)
+        start();
+})
 
